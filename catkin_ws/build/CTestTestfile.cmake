0cbc64e17035c42996a94031ac0e60cf0e0f9976
rkolbert <rkolbert@mit.edu>
2016-10-14 21:23:49 -0400

    trial commit

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -1,10 +1,9 @@
 # CMake generated Testfile for 
-# Source directory: /home/robot/me212lab4/catkin_ws/src
-# Build directory: /home/robot/me212lab4/catkin_ws/build
+# Source directory: /home/robot/me212lab3/catkin_ws/src
+# Build directory: /home/robot/me212lab3/catkin_ws/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 SUBDIRS(gtest)
 SUBDIRS(me212bot)
-SUBDIRS(me212cv)
 SUBDIRS(pr_apriltags)
